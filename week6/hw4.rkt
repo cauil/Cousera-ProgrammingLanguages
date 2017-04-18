811610ae3ba432f5279e7ce9b150eb270e34cabf
licaifa <licaifa@meituan.com>
2017-04-18 20:48:56 +0800

    week4: 5-9hw

--- week6/hw4.rkt
+++ week6/hw4.rkt
@@ -4,19 +4,23 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
+; problem 1
 (define (sequence low high stride)
   (cond [(> low high) null]
         [#t (cons low (sequence (+ low stride) high stride))])
 
+; problem 2
 (define (string-append-map xs suffix)
   (map (lambda (str) (string-append str suffix)) xs))
 
+; problem 3
 (define (list-nth-mod xs n)
   (cond [(< n 0) (error "list-nth-mod: negative number")]
         [(null? xs) (error "list-nth-mod: empty list")]
-        [#t (let ([foo (remainder (length xs) n)])
+        [#t (let ([foo (remainder n (length xs))])
                (car (list-tail xs foo)))]))
 
+; problem 4
 (define (stream-for-n-steps s n)
   (if (= n 0)
     null
@@ -38,6 +42,7 @@
                 (cons x (lambda () (f (fn x arg)))))])
     (lambda () (f 1))))
 
+; problem 5
 (define funny-number-stream 
   (stream-maker (lambda (x y)
                   (let* ([x (if (< x 0) (- 0 x) x)]
@@ -45,3 +50,34 @@
                           (if (= (remainder (+ x y) 5) 0)
                            (- 0 foo)
                            foo))) 1))
+
+; problem 6
+(define (dan-then-dog)
+  (letrec ([f (lambda (x)
+                   (cons x (lambda ()
+                             (f (if (string=? x "dan.jpg") "dog.jpg" x)))))])
+    (f "dan.jpg")))
+
+; problem 7
+(define (stream-add-zero s)
+  (letrec ([f (lambda (s)
+                (cons (cons 0 (car (s))) (lambda () (f (cdr (s))))))])
+    (lambda () (f s))))
+
+; problem 8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (i)
+                (cons (cons (list-nth-mod xs i) (list-nth-mod ys i))
+                      (lambda () (f (+ i 1)))))])
+    (lambda () (f 0))))
+
+; problem 9
+(define (vector-assoc v vec)
+  (letrec ([len (vector-length vec)]
+           [f (lambda (i)
+                (if (< i len)
+                (letrec ([item (vector-ref vec i)])
+                  (cond [(and (pair? item) (equal? (car item) v)) item]
+                        [#t (f (+ i 1))]))
+                #f))])
+    (f 0)))
